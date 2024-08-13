30db8b1244222a56794026fcb49e5e0439ce52b3
PABLO CANDIDO PEREIRA DE LANA <202210393@unifoa.edu.br>
2024-08-12 21:04:20 -0300

    Adicionar arquivos de projeto.

---
+++ Locauto/Controllers/ClienteController.cs
@@ -0,0 +1,45 @@
+﻿using Locauto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locauto.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public static List<Cliente> Clientes = 
+            new List<Cliente>()
+            {
+                new Cliente()
+                {
+                    Id = 1, 
+                    Nome = "Rebeca",
+                    Email = "rebecaandrade@gmail.com",
+                    Telefone = "(21)998347374",
+                    Cidade = "Rio de Janeiro"
+                },
+                new Cliente(){
+                    Id = 2,
+                    Nome = "Medina",
+                    Email = "Medina@gmail.com",
+                    Telefone = "(21)99939735",
+                    Cidade = "São Paulo",
+                },
+
+            };
+        public IActionResult Index()
+        {
+            return View(Clientes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Cliente cliente)
+        {
+            cliente.Id = Clientes.Select(x => x.Id).Max() +1;
+            Clientes.Add(cliente);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Locauto/Controllers/FabricanteController.cs
@@ -0,0 +1,44 @@
+﻿using Locauto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locauto.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        public static List<Fabricante> Fabricantes =
+            new List<Fabricante>()
+            {
+                new Fabricante()
+                {
+                    Id = 1,
+                    Nome = "Travis",
+                    Pais = "Dinamarca",
+                },
+                new Fabricante(){
+                    Id = 2,
+                    Nome = "Pedro",
+                    Pais = "Brasil",
+                },
+
+            };
+        public IActionResult Index()
+        {
+            return View(Fabricantes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Fabricante fabricante)
+        {
+            fabricante.Id = Fabricante.Select(x => x.Id).Max() + 1;
+            Fabricante.Add(fabricante);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+  
+
